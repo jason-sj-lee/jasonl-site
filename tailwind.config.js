65a86b4c997e5b3c33ccafa81e99ac528f57e733
jason-sj-lee <jason.981214@gmail.com>
2024-08-07 17:16:41 -0400

    feat: finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,32 +6,12 @@ module.exports = {
       mono: ["JetBrains Mono", "monospace"],
     },
     extend: {
-      transitionTimingFunction: {
-        spring: "cubic-bezier(.175,.885,.32,1.275)",
-      },
-      keyframes: {
-        "hue-rotate": {
-          "0%": { filter: "hue-rotate(0deg)" },
-          "100%": { filter: "hue-rotate(360deg)" },
-        },
-        "hue-rotate-reverse": {
-          "0%": { filter: "hue-rotate(0deg)" },
-          "100%": { filter: "hue-rotate(-360deg)" },
-        },
-        ping: {
-          "75%, 100%": {
-            transform: "scale(2)",
-            opacity: "0",
-          },
-        },
-      },
-      animation: {
-        "ping-slow": "ping 3s cubic-bezier(0, 0, 0.2, 1) infinite",
-        "hue-rotate": "hue-rotate 1s infinite linear",
-        "hue-rotate-reverse-slow": "hue-rotate-reverse 4s infinite linear",
+      backgroundImage: {
+        mejuri: "url('../public/mejuri_logo.png')",
+        hootsuite: "url('../public/hootsuite_logo.svg')",
+        pk: "url('../public/pk.png')",
       },
     },
-    extend: {},
   },
   plugins: [],
 };
